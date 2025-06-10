61675a0b18d4985d461dad41bf328de902f38213
Brownie-Coder9 <madhubala.mohanakrishnan@gmail.com>
2025-06-10 18:56:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -131,5 +131,5 @@ def analyze():
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     os.makedirs('static', exist_ok=True)
-    app.run(debug=True, host='0.0.0.0', port=(int(os.environ.get("PORT",5000)))
+    app.run(debug=True, host='0.0.0.0', port=(int(os.environ.get("PORT",5000))))
     print("Flask app created")
